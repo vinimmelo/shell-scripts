2eeb2c847c6e64ff1e185d58c753792da19e3a91
Vinícius Melo <viniciusdesk@icloud.com>
2019-03-28 12:10:28 -0300

    Addition of functions to sys_info_page and example echoed into system_report.html

---
+++ CodesComplete/sys_info_page.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Program to output a system information page
+
+TITLE="System Information Report For $HOSTNAME"
+CURRENT_TIME=$(date +"%x %r %Z")
+TIME_STAMP="Generated $CURRENT_TIME, by $USER"
+
+report_uptime () {
+  cat <<- _EOF_
+  <H2>System Uptime</H2>
+  <PRE>$(uptime)</PRE>
+_EOF_
+}
+
+report_disk_space () {
+  cat <<- _EOF_
+  <h2>Disk Space Utilization</h2>
+  <PRE>$(df -h)</PRE>
+_EOF_
+return
+}
+
+report_home_space () {
+  cat <<- _EOF_
+  <h2>Home Space Utilization</h2>
+  <PRE>$(du -sh /home/*)</PRE>
+_EOF_
+  return
+}
+
+cat << _EOF_
+  <HTML>
+    <HEAD>
+    <TITLE>$TITLE</TITLE>
+    </HEAD
+    <BODY>
+      <H1>$TITLE</H1>
+      <P>$TIME_STAMP</P>
+      $(report_uptime)
+      $(report_disk_space)
+      $(report_home_space)
+    </BODY>
+  </HTML
+_EOF_
