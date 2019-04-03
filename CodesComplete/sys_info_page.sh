28236b4be3e209256b1658c62853731fa60f9e43
Vinícius Melo <viniciusdesk@icloud.com>
2019-04-02 23:22:57 -0300

    Addition of read function to programs and improved test with integer.

--- CodesComplete/sys_info_page.sh
+++ CodesComplete/sys_info_page.sh
@@ -22,10 +22,17 @@ return
 }
 
 report_home_space () {
-  cat <<- _EOF_
-  <h2>Home Space Utilization</h2>
-  <PRE>$(du -sh /home/*)</PRE>
+  if [[ $(id -u) -eq 0 ]]; then
+    cat <<- _EOF_
+    <h2>Home Space Utilization</h2>
+    <PRE>$(du -sh /home/*)</PRE>
 _EOF_
+  else
+    cat <<- _EOF_
+    <H2>Home Space Utilization ($USER)</H2>
+    <PRE>$(du -sh $HOME)</PRE>
+_EOF_
+  fi
   return
 }
 
